4f9fe315199b2dee73f76c96217de3e9a7c9dc00
zohaib-laeeq <zohaiblaeeq3@gmail.com>
2026-02-18 14:59:26 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -19,6 +19,8 @@ float getFloatValue() {
 char getCharValue() {
     // Declare a variable var_char of type character and set its value to A.
     //return var_char;
+    char var_char = 'A';
+    return var_char;
 }
 
 int convertFloatToInt(float value) {
